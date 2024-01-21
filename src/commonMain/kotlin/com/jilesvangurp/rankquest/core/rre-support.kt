df2049d6343144d198c98c9486ec08856ed784de
Jilles van Gurp <jillesvangurp@users.noreply.github.com>
2024-01-21 12:28:17 +0100

    add some basic rre support

---
+++ src/commonMain/kotlin/com/jilesvangurp/rankquest/core/rre-support.kt
@@ -0,0 +1,107 @@
+package com.jilesvangurp.rankquest.core
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class RREGain(val gain: Int? = null, val rating: Int? = null)
+
+@Serializable
+data class RREQuery(
+    val template: String,
+    val placeholders: Map<String, String>
+)
+
+@Serializable
+data class RREQueryGroup(
+    val name: String?,
+    val description: String?,
+    val queries: List<RREQuery>,
+    @SerialName("relevant_documents")
+    val relevantDocuments: Map<String, RREGain>
+)
+
+
+@Serializable
+data class RreTopic(
+    val description: String?,
+    @SerialName("query_groups")
+    val queryGroups: List<RREQueryGroup>
+)
+
+@Serializable
+data class RRE(
+    val index: String?,
+    @SerialName("corpora_file")
+    val corporaFile: String?,
+    @SerialName("id_field")
+    val idField: String = "id",
+    val topics: List<RreTopic>
+)
+
+/**
+ * Opinionated conversion. Rre seems to allow combining queries for multiple templates. A ranquest
+ * search configuration does not allow this. So, the conversion assumes that the intention is to
+ * run all test cases with the same configuration.
+ *
+ * To facilitate postprocessing, the template is preserved a as a "_template" value in the search
+ * context. You could group the ratings by template and create separate plugin configurations for each.
+ */
+fun RRE.toRatings(): List<RatedSearch> {
+    var id = 0
+    return topics.flatMap { topic ->
+        topic.queryGroups.flatMap { group ->
+            val ratings = group.relevantDocuments.mapNotNull { (id, gain) ->
+                val rating = gain.gain ?: gain.rating
+                if (rating != null) {
+                    SearchResultRating(documentId = id, rating = rating)
+                } else null
+            }
+            group.queries.map { query ->
+                RatedSearch(
+                    id = (id++).toString(),
+                    ratings = ratings,
+                    searchContext = query.placeholders
+                        .map { (k, v) ->
+                            k.replace("${'$'}","") to v
+                        }.toMap().let {
+                            // add _template variable to context
+                            it + ("_template" to query.template)
+                        },
+                    comment = group.name + "" + group.description
+                )
+            }
+        }
+    }
+}
+
+fun List<RatedSearch>.toRRE(index: String = "ranquest", corporaFile: String = "ranquest", idField: String = "id") {
+
+    val groups = groupBy {
+        it.tags
+    }
+
+    val topics = groups.map { (ts, rs) ->
+        // treat each group of tags as a topic
+        RreTopic(
+            description = ts?.joinToString(",") ?: "Rankquest Rated Searches",
+            queryGroups = rs.map { ratedSearch ->
+                // one query group per rated search
+                RREQueryGroup(
+                    name = ratedSearch.id,
+                    description = ratedSearch.comment,
+                    queries = listOf(
+                        RREQuery(ratedSearch.id, ratedSearch.searchContext.map { (k, v) ->
+                            // rre use a $ prefix
+                            "${'$'}$k" to v
+                        }.toMap())
+                    ),
+                    relevantDocuments = ratedSearch.ratings.map {
+                        it.documentId to RREGain(gain = it.rating)
+                    }.toMap()
+                )
+            }
+        )
+    }
+    RRE(index = index, corporaFile = corporaFile, idField = idField, topics = topics)
+}
---
+++ src/commonTest/kotlin/com/jilesvangurp/rankquest/core/RRESupportTest.kt
@@ -0,0 +1,108 @@
+package com.jilesvangurp.rankquest.core
+
+import io.kotest.assertions.assertSoftly
+import io.kotest.assertions.withClue
+import io.kotest.matchers.collections.shouldHaveAtLeastSize
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldNotContain
+import kotlinx.serialization.encodeToString
+import kotlin.test.Test
+
+class RRESupportTest {
+
+    @Test
+    fun shouldParseRREFormat() {
+        val rre = DEFAULT_JSON.decodeFromString(RRE.serializer(), sampleRre)
+
+        val ratings = rre.toRatings()
+        println(DEFAULT_PRETTY_JSON.encodeToString(ratings))
+
+        ratings shouldHaveSize 5
+        // ids should be unique
+        ratings.map { it.id }.toSet() shouldHaveSize 5
+        assertSoftly {
+            ratings.forEach {
+                withClue(
+                    it
+                ) {
+                    // should strip $
+                    it.searchContext.keys.joinToString(",") shouldNotContain "${'$'}"
+                }
+                // there should be some ratings
+                it.ratings shouldHaveAtLeastSize 1
+            }
+        }
+    }
+}
+
+val sampleRre = """
+{
+  "index": "core1",
+  "corpora_file": "electric_basses.json",
+  "id_field": "id",
+  "topics": [
+    {
+      "description": "Fender basses",
+      "query_groups": [
+        {
+          "name": "Brand search",
+          "description": "The group tests several searches on the Fender brand",
+          "queries": [
+            {
+              "template": "only_q.json",
+              "placeholders": {
+                "${"$"}query": "fender"
+              }
+            },
+            {
+              "template": "only_q.json",
+              "placeholders": {
+                "${"$"}query": "fender Bass"
+              }
+            },
+            {
+              "template": "filter_by_number_of_strings.json",
+              "placeholders": {
+                "${"$"}query": "Fender",
+                "${"$"}strings": 4
+              }
+            }
+          ],
+          "relevant_documents": {
+            "1": {
+              "gain": 3
+            },
+            "2": {
+              "gain": 3
+            }
+          }
+        },
+        {
+          "name": "Jazz bass search",
+          "description": "Several searches on a given model (Jazz bass)",
+          "queries": [
+            {
+              "template": "only_q.json",
+              "placeholders": {
+                "${"$"}query": "jazz"
+              }
+            },
+            {
+              "template": "only_q.json",
+              "placeholders": {
+                "${"$"}query": "Jazz bass"
+              }
+            }
+          ],
+          "relevant_documents": {
+            "1": {
+              "gain": 3
+            }
+          }
+        }
+      ]
+    }
+  ]
+}
+""".trimIndent()
